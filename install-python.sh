e5f48056482a94501c30356399f530f63bfff5bb
Jakub Zika <kuba.zika@email.cz>
2017-06-29 02:22:42 +0200

    Adjust install script

--- install-python.sh
+++ install-python.sh
@@ -2,12 +2,12 @@
 if hash python3.6 2>/dev/null; then
     echo python3.6 is alredy installed
 else
-    sudo apt-get -y install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev build-essential libncursesw5-dev libc6-dev openssl
+    apt-get -y install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev build-essential libncursesw5-dev libc6-dev openssl
     mkdir python3.6
     wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
     tar xzvf Python-3.6.1.tgz
     cd Python-3.6.1/
     ./configure
     make
-    sudo make install
+    make install
 fi
--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-
-python3.6 -m pip install -r requirements.txt
-python3.6 install.py
+if hash python3.6 2>/dev/null; then
+    python3.6 -m pip install -r requirements.txt
+    python3.6 install.py
+else
+    sh install-python.sh
+    source ~/.bashrc
+    python3.6 -m pip install -r requirements.txt
+    python3.6 install.py
+fi
