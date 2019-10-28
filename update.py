d2d59199cc999cfef7583b1505db526a807a5d89
jakubzika <zikajak3@fel.cvut.cz>
2019-10-28 10:10:05 +0100

    Stop service while updating

--- update.py
+++ update.py
@@ -3,10 +3,12 @@ import downloadBuild
 import os
 
 def main():
+    os.system('service farm stop')
     os.system('git pull')
     os.system('python3.7 -m pip install -r requirements.txt')
     os.system('chmod 771 server/main.py')
     downloadBuild.main()
+    os.system('service farm start')
 
 if __name__ == '__main__':
     main()
